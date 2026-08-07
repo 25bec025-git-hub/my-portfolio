d45f45f25e1b6cd885c428bb7a3bb20ce82f9b95
25bec025-git-hub <25BEC025@nith.ac.in>
2026-08-08 02:28:02 +0530

    sirf jaruri file

--- my-first-website/scripts/main.js
+++
@@ -1,8 +0,0 @@
-function updateName() {
-  const name = prompt("Enter a new name");
-  button.textContent = `Cadet : ${name}`;
-}
-
-const button = document.querySelector("button");
-
-button.addEventListener("click", updateName);
